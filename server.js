1f80a2dac64f8a51477651722605fae65bb7850c
bruno <brunocanini1994@gmail.com>
2023-11-09 17:34:49 +0100

    feat writing json

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ dotenv.config();
 const loadAjaxData = require ("./utility/loadAjaxData.js");
 
 const fs = require('fs');
+const path = require("path");
+const { isUtf8 } = require("buffer");
 
 const port = +process.env.PORT || 3000;
 
@@ -16,20 +18,35 @@ const server = http.createServer(function(req, res){
     } else {
             // specifichiamo come risponde il server
             loadAjaxData(function(data){
+                const arrayNorris = readNorris();
                 console.log(data);
-
-                fs.appendFile("./data/norrisDb.json", JSON.stringify(data.value) ,function (err) {
-                    if (err) throw err;
-                    console.log("Saved!");
-                    });
+                arrayNorris.push(data.value)
+                console.log(arrayNorris)
 
                 res.writeHead(200, { "Content-Type": "text/html; charset=utf-8"});
                 res.end(data.value);
+
+                fs.writeFileSync("./data/norrisDb.json", JSON.stringify(arrayNorris));
             });
 
     }
 })
 
 server.listen(port, function(){
-    console.log("server is running on port http://localhost:" + port)
+    console.log("server is running on port http://localhost:" + port);
 })
+
+function readNorris(){
+
+    const norrisPath = path.join(__dirname, "data", "norrisDb.json");
+
+    try{
+        const norris = fs.readFileSync(norrisPath, "utf-8");
+        // converto il jason in un array di oggetti javascript
+        return JSON.parse(norris);
+    }catch(err){
+        console.log(err)
+        return []
+    }
+
+};
